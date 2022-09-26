dbd5beb6707bf1dcf09a8f89d37554262a1bbcea
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-09-26 17:42:54 +0300

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get '/birds', to: 'birds#index'
+  # get '/birds', to: 'birds#show'
+
+  resources :birds
 end
